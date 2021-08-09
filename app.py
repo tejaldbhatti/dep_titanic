52456adf22d4c2d45dbb93913479eb0e40ef4dd7
tejaldbhatti <82153829+tejaldbhatti@users.noreply.github.com>
2021-08-09 07:43:50 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,40 +1,43 @@
-from flask import Flask, render_template , request
-import joblib
+from flask import Flask, render_template , request 
+import pickle
 
-# initialse the app
 app = Flask(__name__)
 
 #load the model
-model = joblib.load('dib_79.pkl')
+LR_model = pickle.load(open('titanic_train.pkl','rb'))
 
 @app.route('/')
 def hello():
     return render_template('form.html')
 
-@app.route('/submit' , methods = ["POST"])
+
+
+@app.route('/submit', methods = ['POST'])
 def form_data():
-   
-   preg = request.form.get('preg')
-   plas = request.form.get('plas')
-   pres = request.form.get('pres')
-   skin = request.form.get('skin')
-   mass = request.form.get('mass')
-   pedi = request.form.get('pedi')
-   age = request.form.get('age')
-   test = request.form.get('test')
+    Pclass = request.form.get('Pclass')
+    Age = request.form.get('Age')
+    SibSp = request.form.get('SibSp')
+    Parch = request.form.get('Parch')
+    Fare = request.form.get('Fare')
+    Sex_female = request.form.get('Sex_female')
+    Sex_male = request.form.get('Sex_male')
+    Embarked_C = request.form.get('Embarked_C')
+    Embarked_Q = request.form.get('Embarked_Q')
+    Embarked_S = request.form.get('Embarked_S')
+    Embarked_s = request.form.get('Embarked_s')
+    Title_Miss = request.form.get('Title_Miss')
+    Title_Mr = request.form.get('Title_Mr')
+    Title_Mrs = request.form.get('Title_Mrs')
+    Title_Others = request.form.get('Title_Others')
 
-   # insert in db query
-   output = model.predict([[preg,plas,pres,skin,test,mass,pedi,age ]])
-
-   print(output)
-
-   if output[0] == 1:
-        out = 'diabatic'
-   else:
-        out = 'not diabatic'
+    val_prediction = LR_model.predict([[Pclass,Age,SibSp,Parch,Fare,Sex_female,Sex_male,Embarked_C,Embarked_Q,Embarked_S,Embarked_s,Title_Miss,Title_Mr,Title_Mrs,Title_Others]])
+    if val_prediction == 0:
+        out = "Not Survived"
+    else:
+        out = 'Survived'
 
 
-   return render_template('predict.html' , data = f'person is {out}')
+    return render_template('predict.html', data = f'person is {out}')
 
 if __name__ == '__main__':
     app.run(app.run(host='0.0.0.0', port=8080))
